1c9359097df666dd1287cc77dc9e60498665674f
Emerson Junior <emersonjr25@hotmail.com>
2023-03-31 22:18:42 -0300

    organized code and ready to calculate hedges g effect

--- scripts/Doctoral_project_chapter_2.R
+++ scripts/Doctoral_project_chapter_2.R
@@ -14,56 +14,46 @@ library(here)
 ### READING DATA ###
 
 dados <- read.csv("data/raw/Database.csv", header = TRUE, sep = ",")
-subdata <- dados %>% select(paper_no, genus, species, population, simp_trait, T, mean)
+subdata <- dados %>% select(paper_no, genus, species, simp_trait, T, mean)
 subdata$species_complete <- paste0(subdata$genus," ", subdata$species)
+subdata <- subdata %>% select(paper_no, species_complete,  simp_trait, T, mean)
 
-### To calculate Hedge's we need a list with studies, species, traits, temperature and traits ####
+### To calculate Hedge's we need a group with studies, species, and traits ####
 
-max_values <- subdata %>%
-  as.tibble() %>%
-  group_by(paper_no, species_complete, simp_trait) %>%
-  filter(T == max(T))
-  
-min_values <- subdata %>%
-  as.tibble() %>%
-  group_by(paper_no, species_complete, simp_trait) %>%
-  filter(T == min(T))
-
-length_max <- max_values %>%
-  summarise(length(T))
-
-length_min <- min_values %>%
-  summarise(length(T))
-
-mean_max_values <- max_values %>%
-  select(paper_no, 
-         species_complete, 
-         simp_trait,
-         T,
-         mean) %>%
-  mutate(mean = as.double(mean)) %>%
-  group_by(paper_no, 
-           species_complete, 
-           simp_trait) %>%
-  summarise(mean(mean))
-
-mean_min_values <- min_values %>%
-  select(paper_no, 
-         species_complete, 
-         simp_trait,
-         T,
-         mean) %>%
-  mutate(mean = as.double(mean)) %>%
-  group_by(paper_no, 
-           species_complete, 
-           simp_trait) %>%
-  summarise(mean(mean))
-
-result <- mean_max_values %>%
-  select(-`mean(mean)`)
-
-hedges <- function(mean_min, mean_max, group_1, group_2){
-   abs((mean_min - mean_max) / sd_pooled(group_1, group_2))
+### FUNCTIONS ###
+temp_ampli <- function(x, f){
+   x %>%
+    as.tibble() %>%
+    group_by(paper_no, species_complete, simp_trait) %>%
+    filter(T == f(T))
 }
 
-result$hedges_g <- mapply(hedges, mean_min_values$`mean(mean)`, mean_max_values$`mean(mean)`, length_min$`length(T)`, length_max$`length(T)`)
+length_temp <- function(x, f){
+  x %>%
+    summarise(length = f(T))
+} 
+
+mean_temp <- function(x, f){
+  x %>%
+    mutate(mean = as.double(mean)) %>%
+    summarise(mean_variable = f(mean))
+}
+
+hedges <- function(mean_min, mean_max, group_1, group_2){
+  abs((mean_min - mean_max) / sd_pooled(group_1, group_2))
+}
+
+### CALCULATION ###
+max_values <- temp_ampli(subdata, max)
+min_values <- temp_ampli(subdata, min)
+
+length_max <- length_temp(max_values, length)
+length_min <- length_temp(min_values, length)
+
+mean_max_values <- mean_temp(max_values, mean)
+mean_min_values <- mean_temp(min_values, mean)
+
+result <- mean_max_values %>%
+  select(-mean_variable)
+
+result$hedges_g <- mapply(hedges, mean_min_values$mean_variable, mean_max_values$mean_variable, length_min$length, length_max$length)
