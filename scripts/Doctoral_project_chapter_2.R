9c98df015a0a0b3f7fe0146f4cdef530c60cc2d6
emersonjr25 <emersonjr25@hotmail.com>
2023-09-29 16:27:38 -0300

    results saved with markov 3000 timesteps

--- scripts/Doctoral_project_chapter_2.R
+++ scripts/Doctoral_project_chapter_2.R
@@ -241,12 +241,12 @@ w <- diff(sapply(preliminar[2:(ncol(preliminar) -1)], quantile, c(0.05, 0.95)))
 
 mcmc_result <- mcmc(musse_ordered, 
                     init[colnames(w)], 
-                    nsteps=1000,prior=prior, 
+                    nsteps=3000,prior=prior, 
                     w=w, print.every=10)
 #saveRDS(mcmc_result, file="mcmc.rds")
 
 # remove burn-in #
-mcmc_result <- readRDS("mcmc.rds")
+#mcmc_result <- readRDS("mcmc.rds")
 mcmc_max <- nrow(mcmc_result)
 mcmc_out_burn_in <- round(nrow(mcmc_result) * 0.2) + 1
 mcmc_result <- mcmc_result[mcmc_out_burn_in:mcmc_max, ]
@@ -282,14 +282,27 @@ mcmc_result_pivoted <- mcmc_result_pivoted |>
 ### plots ###
 
 # temporal series #
-ggplot(mcmc_result, aes(i, p)) +
+save_result <- TRUE
+markov <- ggplot(mcmc_result, aes(i, p)) +
   geom_line() + 
   xlab('Time') + ylab('Log(L)') +
   theme_bw() +
   theme(axis.title.x = element_text(size = 14), 
         axis.title.y = element_text(size = 14))
 
-ggplot(mcmc_result, aes(x=i)) +
+if(save_result == TRUE){
+  tiff(filename = file.path(here('output'), paste0('markov_result', ".tif")),
+       #width = 1000,
+       width = 800, #without abundance and occupancy
+       height = 600,
+       #height = 400, #abundance and occupancy
+       units = "px",
+       res = 100)
+  print(markov)
+  dev.off()
+}
+
+markov_lambda <- ggplot(mcmc_result, aes(x=i)) +
   geom_line(aes(y=lambda1), color='blue') +
   geom_line(aes(y=lambda2), color='black') +
   geom_line(aes(y=lambda3), color='red') +
@@ -297,6 +310,18 @@ ggplot(mcmc_result, aes(x=i)) +
   theme_bw() +
   theme(axis.title.x = element_text(size = 14), 
         axis.title.y = element_text(size = 14))
+
+if(save_result == TRUE){
+  tiff(filename = file.path(here('output'), paste0('markov_result_lambda', ".tif")),
+       #width = 1000,
+       width = 800, #without abundance and occupancy
+       height = 600,
+       #height = 400, #abundance and occupancy
+       units = "px",
+       res = 100)
+  print(markov_lambda)
+  dev.off()
+}
 
 # transitions #
 ggplot(transitions, aes(value, fill = name)) +
@@ -309,7 +334,7 @@ ggplot(transitions, aes(value, fill = name)) +
   xlab("Transition") + ylab('Posterior Density') 
 
 # speciation #
-mcmc_result_pivoted %>% 
+speciation <- mcmc_result_pivoted %>% 
   ggplot(aes(LambdaPosterior, fill = Speciation)) + 
   geom_density(alpha=0.7) +
   theme_bw() +
@@ -322,8 +347,20 @@ mcmc_result_pivoted %>%
         axis.title.y = element_text(size = 14)) +
   xlab("Speciation") + ylab('Posterior Density') 
 
+if(save_result == TRUE){
+  tiff(filename = file.path(here('output'), paste0('speciation', ".tif")),
+       #width = 1000,
+       width = 800, #without abundance and occupancy
+       height = 600,
+       #height = 400, #abundance and occupancy
+       units = "px",
+       res = 100)
+  print(speciation)
+  dev.off()
+}
+
 # extinction #
-mcmc_result_pivoted %>% 
+extinction <- mcmc_result_pivoted %>% 
   ggplot(aes(ExtinctionPosterior, fill = Extinction)) + 
   geom_density(alpha=0.7) +
   theme_bw() +
@@ -336,8 +373,20 @@ mcmc_result_pivoted %>%
         axis.title.y = element_text(size = 14)) +
   xlab("Extinction") + ylab('Posterior Density') 
 
+if(save_result == TRUE){
+  tiff(filename = file.path(here('output'), paste0('extinction', ".tif")),
+       #width = 1000,
+       width = 800, #without abundance and occupancy
+       height = 600,
+       #height = 400, #abundance and occupancy
+       units = "px",
+       res = 100)
+  print(extinction)
+  dev.off()
+}
+
 # diversification #
-mcmc_result_pivoted %>% 
+diversification <- mcmc_result_pivoted %>% 
   ggplot(aes(DiversificationPosterior, fill = Diversification)) + 
   geom_density(alpha=0.7) +
   theme_bw() +
@@ -350,12 +399,35 @@ mcmc_result_pivoted %>%
         axis.title.y = element_text(size = 14)) +
   xlab("Diversification") + ylab('Posterior Density') 
 
+if(save_result == TRUE){
+  tiff(filename = file.path(here('output'), paste0('diversification', ".tif")),
+       #width = 1000,
+       width = 800, #without abundance and occupancy
+       height = 600,
+       #height = 400, #abundance and occupancy
+       units = "px",
+       res = 100)
+  print(diversification)
+  dev.off()
+}
+
 # tree per states #
-color.palette = colorRampPalette(c("blue", "#ffcd74", "#ff7251"))
-plotBranchbyTrait(resolved_tree_ncbi, 
-                  hedgesg, mode=c("edges"),
-                  palette=color.palette,
-                  legend=FALSE)
+color.palette = colorRampPalette(c("#ffcd74", "#ff7251", "#9b2948"))
+
+if(save_result == TRUE){
+  tiff(filename = file.path(here('output'), paste0('tree_states', ".tif")),
+       #width = 1000,
+       width = 800, #without abundance and occupancy
+       height = 600,
+       #height = 400, #abundance and occupancy
+       units = "px",
+       res = 100)
+  print(plotBranchbyTrait(resolved_tree_ncbi, 
+                          hedgesg, mode=c("edges"),
+                          palette=color.palette,
+                          legend=FALSE))
+  dev.off()
+}
 
 ################################################################################
 ##### MUSSE TO MORE FREQUENT TRAIT #######################
